0551c85cd9f3446afb0f61df7d1bb09743948bf7
sanflog <daichan613@gmail.com>
2023-01-26 00:16:09 +0900

    test

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
+ };
