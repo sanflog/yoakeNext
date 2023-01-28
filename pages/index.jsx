85275e17923a82f43243d1422a27c35bdb309eb3
sanflog <daichan613@gmail.com>
2023-01-28 12:21:07 +0900

    Make index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,27 +1,19 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-import Layout from '../components/layout';
-
-import { useState } from "react";
+import Layout, {siteTitle} from '../components/layout';
+import utilStyles from '/styles/utils.module.css';
 
 export default function HomePage() {
-  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
-  const [likes, setLikes] = useState(0);
-
-  function handleClick() {
-    setLikes(likes + 1);
-  }
-
   return (
-		<Layout>
+		<Layout home>
 			<Head>
-				<title>Yoake</title>
+				<title>{siteTitle}</title>
 			</Head>
-			<h1>Yoake</h1>
-			<h1>Sasaki Yudai</h1>
-			<p>I am web designer.</p>
-			This is <Link href="/tests/test_page">test page links!</Link>
+			<section className={utilStyles.headingMd}>
+				<p>Hello, I'm Yudai. I'm learning web development these days. You can contact me on <a href="https://twitter.com/SasakiYudai0">Twitter</a></p>
+				<p>
+				This is <a href="/tests/test_page">test page links!</a>
+				</p>
+			</section>
 		</Layout>
   );
 }
--- pages/tests/test_page.jsx
+++ pages/tests/test_page.jsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import Head from 'next/head';
-import Image from 'next/image';
 import Layout from '../../components/layout';
 
 export default function FirstTest() {
@@ -10,8 +9,6 @@ export default function FirstTest() {
 				<title>Yoake</title>
 			</Head>
 			<h1>First test</h1>
-			<Image src="/images/profile.jpg" height={144} width={144} alt='sasaki' />
-			<Link href="/">Back to home</Link>
 		</Layout>
 	);
 }
