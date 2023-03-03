124239586240427b787f9735daa5cfe63670e946
sanflog <daichan613@gmail.com>
2023-03-03 23:48:12 +0900

    make_function_cards

--- pages/thinkingAnalyzer/thinkingAnalyzer.jsx
+++ pages/thinkingAnalyzer/thinkingAnalyzer.jsx
@@ -1,38 +1,103 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
+import { useState } from 'react';
+
 import Layout from '/components/layout';
-import Date from '/components/date';
-import { getSortedPostsData } from '/lib/posts';
 
 import utilStyles from '/styles/utils.module.css';
 import styles from './thinkingAnalyzer.module.css';
 
-export async function getStaticProps() {
-	const allPostsData = getSortedPostsData();
-	return {
-		props: {
-			allPostsData,
-		}
-	};
+const list = [
+	{ 
+		'title' : 'Test Title',
+		'detail' : [
+			{
+				'target' : 'What',
+				'function' : 'Percieve',
+				'type' : 'Sight',
+				'time' : 'Furture',
+				'decideTo' : 'none',
+			},
+			{
+				'target' : 'Pepar',
+				'function' : 'Judgment',
+				'type' : 'Feeling',
+				'time' : 'none',
+				'decideTo' : 'Value',
+			}
+		]
+	},
+	{ 
+		'title' : 'Hello World',
+		'detail' : [
+			{
+				'target' : 'What',
+				'function' : 'Judging',
+				'type' : 'Thinking',
+				'time' : 'none',
+				'decideTo' : 'Reason',
+			}
+		]
+	}
+];
+
+function cardTitleHandler( showCard, setShowCard ) {
+	setShowCard(!showCard);
 }
 
-function FunctionCard() {
-	return (
-
-		<div className={ styles.thinkingAnalyzerCard }>
-			<h3>Make documents for job</h3>
-			<div className={ styles.thinkingAnalyzerList }>
-				<p>Furture Percieve to future target</p>
-				<p>Past Percieve to actual results</p>
-				<p>Now Percieve to now target</p>
-				<p>Thinking decision to what I should wright</p>
+function FunctionCard({item}) {
+	const [showCard, setShowCard] = useState(false);
+	const details = item.detail.map((i, n) => {
+		return (
+			<div>
+				<div className={ styles.detailTarget }>{n} Target: {i.target}</div>
+				<table className={ styles.detailTable }>
+					<tr>
+						<th>Function</th>
+						<th>Type</th>
+						<th>Time</th>
+						<th>Decide To</th>
+					</tr>
+					<tr>
+						<td>{i.function}</td>
+						<td>{i.type}</td>
+						<td>{i.time}</td>
+						<td>{i.decideTo}</td>
+					</tr>
+				</table>
 			</div>
-		</div>
-	); 
+		);
+	});
+
+	if (!showCard) {
+		return (
+			<div className={ styles.thinkingAnalyzerCard }>
+				<a onClick={ () => cardTitleHandler(showCard, setShowCard) }>
+					<div>{item.title}</div>
+				</a>
+			</div>
+		); 
+	} else {
+		return (
+			<div className={ styles.thinkingAnalyzerCard }>
+				<a onClick={ () => cardTitleHandler(showCard, setShowCard) }>
+					<div>{item.title}</div>
+				</a>
+
+				<div className={ styles.thinkingAnalyzerList }>
+					{details}
+				</div>
+			</div>
+		); 
+	}
 }
 
 export default function ThinkingAnalyzer({allPostsData}) {
+	const cards = list.map((lst) => {
+		return <FunctionCard item={lst} />;
+	});
+
 	return (
 		<Layout>
 			<section>
@@ -42,9 +107,7 @@ export default function ThinkingAnalyzer({allPostsData}) {
 				</Head>
 
 				<h1>Thinking Analyzer</h1>
-
-				<FunctionCard />
-				<FunctionCard />
+				{cards}
 
 			</section>
 		</Layout>
