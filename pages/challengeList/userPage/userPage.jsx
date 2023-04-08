7140c6f2fc9eab3e7d5ac8f0cb2a7f554a709ff8
sanflog <daichan613@gmail.com>
2023-04-08 23:22:28 +0900

    something

--- pages/challengeList/userPage/userPage.jsx
+++ pages/challengeList/userPage/userPage.jsx
@@ -8,13 +8,16 @@ import styles from './userPage.module.css';
 import utilStyles from '../../../styles/utils.module.css';
 
 
-function signout(setSignedIn) {
-	document.cookie = 'username= ;';
-	setSignedIn('');
-}
+{/*
+	* Contact with server process
+	* */}
 
 
 function achieveChangeHandler(id, value) {
+	{/*
+		* The function to update an achieve status of a challenge item
+		* */}
+
 	fetch('https://yoake.herokuapp.com/challengeList/changeAchieve/', {
 		method: 'POST',
 		headers: {
@@ -25,7 +28,12 @@ function achieveChangeHandler(id, value) {
 		.catch(e => console.error(e))
 }
 
+
 function deleteItem(id, setResMsg) {
+	{/*
+		* The function to delete a challenge item
+		* */}
+
 	fetch('https://yoake.herokuapp.com/challengeList/deleteItem/', {
 		method: 'POST',
 		headers: {
@@ -38,7 +46,12 @@ function deleteItem(id, setResMsg) {
 		.catch(e => console.error(e))
 }
 
+
 function deleteList(id, setResMsg) {
+	{/*
+		* The function to delete a challenge list
+		* */}
+
 	const data = { 'id' : id };
 	fetch('https://yoake.herokuapp.com/challengeList/deleteList/', {
 		method: 'POST',
@@ -52,12 +65,16 @@ function deleteList(id, setResMsg) {
 		.catch(e => console.error(e))
 }
 
+
+{/*
+	* Return response message for delete item or list
+	*/}
+
 function ResponseMessage({resMsg}) {
 	if (resMsg !== '') { 
 		return (
 			<div>
 				<p className={styles.resMsg}>{resMsg}</p>
-				<button onClick={() => location.reload()}>you should reload page</button>
 			</div>
 		);
 	} else {
@@ -65,14 +82,34 @@ function ResponseMessage({resMsg}) {
 	}
 }
 
+{/*
+	* Sign out process	
+	*/}
+
+function signout(setSignedIn) {
+	document.cookie = 'username= ;';
+	setSignedIn('');
+}
+
+
+{/* 
+	* MAIN PROCESS	
+	*/}
+
 export default function UserPage({ 
 	username, 
 	setSignedIn,
 }) {
+
+	{/* Declare variables  */}
+
 	const [showCreateList, setShowCreateList] = useState(false);
 	const [challengeLists, setChallengeLists] = useState([]);
 	const [challengeItems, setChallengeItems] = useState([]);
 	const [resMsg, setResMsg] = useState('');
+
+
+	{/* Get user's list data from server  */}
 
 	useEffect(() => {
 		fetch('https://yoake.herokuapp.com/challengeList/userpage/?username=' + username)
@@ -85,35 +122,57 @@ export default function UserPage({
 		setResMsg('');
 	}, []);
 
+
+	{/* Show create list handler */}
+
 	function createListHandler() {
 		setShowCreateList(!showCreateList);
 	}
 
+
+	{/* Make checkbox input by the condition of isAchieve property  */}
+
 	function Checkbox({list, item}) {
 		const [value, setValue] = useState(item.isAchieve)
+
 		if (list.id == item.listName_id) {
 			const checkbox = () => {
-				if (!item.isAchieve) {
+
+				if (!value) {
+
+					{/* Generate checkbox */}
+
 					return (
 						<input 
 							className={styles.checkitem} 
 							type="checkbox" 
 							value={!value}	
-							onChange={(e) => achieveChangeHandler(item.id, !value)}
+							onChange={(e) => {
+								achieveChangeHandler(item.id, !value);
+								setValue(!value);
+							}}
 						/>
 					);
+
 				} else {
+
 					return (
 						<input 
 							className={styles.checkitem} 
 							type="checkbox" 
 							value={!value}	
-							onChange={(e) => achieveChangeHandler(item.id, !value)}
+							onChange={(e) => {
+								achieveChangeHandler(item.id, !value);
+								setValue(!value);
+							}}
 							checked
 						/>
 					);
 				}
 			}
+
+			{/* Create item line */}
+
 			return (
 				<div key={item.id}>
 					<li className={styles.item}>
@@ -130,6 +189,9 @@ export default function UserPage({
 			);
 		}
 	}
+
+
+	{/* Generate challenge list */}
 
 	const lists = challengeLists.map((list) => {
 		const items = challengeItems.map((item) => {
@@ -160,6 +222,8 @@ export default function UserPage({
 			</div>
 		);
 	});
+
+	{/* Return entire of the user page  */}
 
 	return (
 		<div>
