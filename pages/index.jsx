4dc3647e17cbecb07e40b0a788bc990531a4a98f
sanset <38806716+sanflog@users.noreply.github.com>
2024-01-26 14:42:52 +0000

    ll

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,7 +23,6 @@ export async function getStaticProps() {
 export default function HomePage({ allPostsData }) {
   return (
     <>
-      <LoadPage />
       <Layout>
         <Head>
           <meta
