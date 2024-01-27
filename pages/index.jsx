6d3358eee476b4d136e3b6b57ac6386c9ff08253
sanset <38806716+sanflog@users.noreply.github.com>
2024-01-27 04:05:25 +0000

    aa

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,40 +1,33 @@
 import Head from "next/head";
 import Link from "next/link";
 
-import Layout, { siteTitle } from "../components/layout";
-
 import utilStyles from "/styles/utils.module.css";
 import styles from "./index.module.css";
 
 export default function HomePage({}) {
   return (
     <>
-      <Layout>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <title>{siteTitle}</title>
-        </Head>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>YOAKE</title>
+      </Head>
 
-        <section className={` ${utilStyles.padding10px}`}>
-          <div
-            className={` ${utilStyles.headingX1} ${utilStyles.padding1px} ${utilStyles.headerFont}`}
-          >
-            <h1>
-              YUDAI SAIKAWA IS A WEB DESIGNER AND VIDEO CREATOR.
-              <Link href="http://yoake.lsv.jp"> MY WORKS ARE HERE.</Link>
-            </h1>
-            <h1>
-              IF YOU HAVE ANY QUESTIONS,a
-              <Link href="https://www.instagram.com/yoakeed/">
-                PLEASE LET ME KNOW.
-              </Link>
-            </h1>
-          </div>
-        </section>
-      </Layout>
+      <section className={` ${utilStyles.padding10px}`}>
+        <div
+          className={` ${utilStyles.headingX1} ${utilStyles.padding1px} ${utilStyles.headerFont}`}
+        >
+          <h1>
+            YUDAI SAIKAWA IS A WEB DESIGNER AND VIDEO CREATOR.
+            <Link href="http://yoake.lsv.jp"> MY WORKS ARE HERE.</Link>
+          </h1>
+          <h1>
+            IF YOU HAVE ANY QUESTIONS,a
+            <Link href="https://www.instagram.com/yoakeed/">
+              PLEASE LET ME KNOW.
+            </Link>
+          </h1>
+        </div>
+      </section>
     </>
   );
 }
